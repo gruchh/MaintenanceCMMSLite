cd7c27495673beac60fb1f1eda6bec9b9932cc5d
gruchh <kubol2@gmail.com>
2025-07-30 17:31:25 +0200

    Initial commit

---
+++ backend/src/main/java/com/cmms/lite/CmmsLiteApplication.java
@@ -0,0 +1,13 @@
+package com.cmms.lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmmsLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmmsLiteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cmms/lite/CmmsLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmms.lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmmsLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
