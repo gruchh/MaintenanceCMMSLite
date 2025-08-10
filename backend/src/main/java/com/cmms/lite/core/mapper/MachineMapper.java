1380797aedd13ad4ef7dc31c10d418a66f1536cb
gruchh <kubol2@gmail.com>
2025-08-10 18:55:11 +0200

    Inproper all machines mapstruct mapper fix

--- backend/src/main/java/com/cmms/lite/core/mapper/MachineMapper.java
+++ backend/src/main/java/com/cmms/lite/core/mapper/MachineMapper.java
@@ -3,18 +3,24 @@ package com.cmms.lite.core.mapper;
 import com.cmms.lite.api.dto.MachineDTOs;
 import com.cmms.lite.core.entity.Machine;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
-@Mapper(
-        componentModel = "spring"
-)
+@Mapper(componentModel = "spring")
 public interface MachineMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "breakdownsList", ignore = true)
 
     Machine toEntity(MachineDTOs.CreateRequest request);
     MachineDTOs.Response toResponse(Machine machine);
     List<MachineDTOs.Response> toResponseList(List<Machine> machines);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "breakdownsList", ignore = true)
     void updateEntityFromRequest(MachineDTOs.UpdateRequest request, @MappingTarget Machine machine);
+
     MachineDTOs.SummaryResponse toSummaryResponse(Machine machine);
 }
