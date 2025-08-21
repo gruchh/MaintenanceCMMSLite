009bc781303b76e0ec8ea7277f4438d2c7a6bf6b
gruchh <kubol2@gmail.com>
2025-08-21 18:01:35 +0200

    Fix problem with not initializing login status after refresh

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core'; // <-- Dodaj OnInit
 import { CommonModule } from '@angular/common';
 import { LoginModalService } from './core/api/login-modal.service';
 import { LoginModalComponent } from './shared/components/login-modal/login-modal.component';
+import { Router, RouterOutlet } from '@angular/router';
+import { AuthService } from './core/api/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,16 @@ import { LoginModalComponent } from './shared/components/login-modal/login-modal
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   private loginModalService = inject(LoginModalService);
   private router = inject(Router);
+  private authService = inject(AuthService);
 
   isLoginModalOpen = this.loginModalService.isModalOpen;
+
+  ngOnInit(): void {
+    this.authService.initializeAuthState().subscribe();
+  }
 
   onModalClose(isSuccess: boolean): void {
     this.loginModalService.close();
